483721cdfdfddc266aedf4a93eb2456737bbd2ed
masput <prajaputra98@gmail.com>
2022-11-25 13:16:34 +0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">MASTECH</Navbar.Brand>
+        <Navbar.Brand href="#home">Sekolah Menengah Atas</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            made with love 2022
+            Made with love 2022
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
